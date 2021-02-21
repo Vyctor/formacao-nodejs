0456cc996a3a08312f16d66cf31b9d0e2b735f7d
Vyctor <dev.vyctor@gmail.com>
2021-02-21 16:33:15 -0300

    estruturas condicionais com EJS

---
+++ guia-perguntas/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// Dizendo para o Express usar o EJS como View Engine
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  var nome = "Vyctor Guimarães";
+  var lang = "JavaScript";
+
+  res.render("index", { nome, lang });
+});
+
+app.listen(8080, () => {
+  console.log("App rodando!");
+});
