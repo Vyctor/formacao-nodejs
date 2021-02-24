7ce1918c64f68222c42a81b58d5b70c0c49cd60f
Vyctor <dev.vyctor@gmail.com>
2021-02-24 20:16:11 -0300

    adicionando o bootstrap ao projeto

--- guia-perguntas/index.js
+++ guia-perguntas/index.js
@@ -6,17 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const nome = "Vyctor Guimarães";
-  const lang = "JavaScript";
-  const error = false;
-
-  const produtos = [
-    { nome: "Doritos", preco: 3.14 },
-    { nome: "Coca Cola", preco: 5.9 },
-    { nome: "Leite", preco: 3.15 },
-  ];
-
-  res.render("index", { nome, lang, error, produtos });
+  res.render("index");
 });
 
 app.listen(8080, () => {
