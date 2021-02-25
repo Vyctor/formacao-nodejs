05ce0bfb753e6d57c921e31000dbcdf7baa9d0ae
Vyctor <dev.vyctor@gmail.com>
2021-02-24 21:02:08 -0300

    capturando dados de um formulario

--- guia-perguntas/index.js
+++ guia-perguntas/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Dizendo para o Express usar o EJS como View Engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -14,7 +20,9 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarPergunta", (req, res) => {
-  res.send("Formulário recebido");
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send(`Formulário recebido! ${titulo} - ${descricao}`);
 });
 
 app.listen(8080, () => {
