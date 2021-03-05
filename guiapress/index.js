a1ef1d000d2dbbb6b32e43682c6eb344f8fb09d7
Vyctor <dev.vyctor@gmail.com>
2021-03-05 06:40:28 -0300

    base do projeto

---
+++ guiapress/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const jsonParser = require("body-parser/lib/types/json");
+const urlencodedParser = require("body-parser/lib/types/urlencoded");
+const connection = require("./database/database");
+
+// View engine
+app.set("view engine", "ejs");
+
+// Static
+app.use(express.static("public"));
+
+// Body parser
+app.use(urlencodedParser({ extended: false }));
+app.use(jsonParser());
+
+// Db connection
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(8080, () => {
+  console.log("App rodando...");
+});
