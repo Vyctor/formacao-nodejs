49a00669664192697360c46c3e95724e66ee396d
Vyctor <dev.vyctor@gmail.com>
2021-03-07 20:03:00 -0300

    feat: tabelas do bootstrap

--- guiapress/categories/CategoriesController.js
+++ guiapress/categories/CategoriesController.js
@@ -21,4 +21,8 @@ router.post("/admin/categories/save", (req, res) => {
     }
 });
 
+router.get("/admin/categories", (req, res) => {
+    res.render("admin/categories/index");
+});
+
 module.exports = router;
