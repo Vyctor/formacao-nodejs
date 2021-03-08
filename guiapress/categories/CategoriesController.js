81c2ec63b1ce1df90085eef1ac4d4fd3e5acc408
Vyctor <dev.vyctor@gmail.com>
2021-03-07 21:08:22 -0300

    feat: linkando botoes de edicao

--- guiapress/categories/CategoriesController.js
+++ guiapress/categories/CategoriesController.js
@@ -44,4 +44,24 @@ router.post("/admin/categories/delete", (req, res) => {
     }
 });
 
+router.get("/admin/categories/edit/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id)
+        .then((category) => {
+            if (category != undefined) {
+                res.render("admin/categories/edit", { category: category });
+            } else {
+                res.redirect("/admin/categories");
+            }
+        })
+        .catch((error) => {
+            res.redirect("/admin/categories");
+        });
+});
+
 module.exports = router;
